ae582b66f5d340ab6ae04991ee500709cf3e7138
jameskyokushin <james@kyokushin.com.ph>
2011-10-03 15:52:48 +0800

    install active admin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AsiPrivate::Application.config.secret_token = '73b805154325cbda314ec7b9920abd2392bdb8f895b25c373e9cacb2e0a9ee93904c2f8b53ca2b6dc87f9c11f18d8b80d3fc6afd3f1f58d7a7337e9d62cca713'
